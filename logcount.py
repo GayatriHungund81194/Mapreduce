8c17ca324eab6ee533c1c943a729d98977da5bd8
root <gayatri811.gh@gmail.com>
2019-04-27 21:15:38 +0000

    code changes

--- logcount.py
+++ logcount.py
@@ -1,10 +1,15 @@
 from pyspark import SparkContext
 from operator import add
- 
+import time
+
+start = time. time()
 sc = SparkContext()
 text  = sc.textFile('input').flatMap(lambda line: line.split(','))
-text1 = text.map(lambda kval : (kval,1) if kval == "200" or kval=="206" or kval=="302" or kval=="304" else None)
-out = text1.collect()
+text1 = text.map(lambda kval : (kval,1) if kval == "200" or kval=="206" or kval=="302" or kval=="304" else (kval,0))
+text2 = text1.reduceByKey(add)
+out = text2.collect()
+end = time. time()
+print("##############Time:",(end-start))
 for o in out:
     print("@@@@@@@@@@@@Output",o)
 
