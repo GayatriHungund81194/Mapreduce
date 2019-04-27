be4cc9900ff4094f1b6708b4bbbd8b8566340850
root <gayatri811.gh@gmail.com>
2019-04-27 05:25:53 +0000

    pyspark

---
+++ logcount.py
@@ -0,0 +1,10 @@
+from pyspark import SparkContext
+from operator import add
+ 
+sc = SparkContext()
+text  = sc.textFile('input').flatMap(lambda line: line.split(','))
+text1 = text.map(lambda kval : (kval,1) if kval == "200" or kval=="206" or kval=="302" or kval=="304" else None)
+out = text1.collect()
+for o in out:
+    print("@@@@@@@@@@@@Output",o)
+
